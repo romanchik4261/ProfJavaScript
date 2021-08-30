a71dded876a5cc91d5dc816104a9f269d2e82f37
romanchik4261 <romanchik4261@mail.ru>
2021-08-30 14:43:49 +0300

    Подключила файлик с товарами

--- js.js
+++ js.js
@@ -1,21 +1,35 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/'; //тут путь
+
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
-        this.fetchProducts(); //метод наполняет массив products товарами
-        this.render(); //вывод товаров на страницу 
+        this._getProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render() //вывод товаров на страницу 
+            });
+
         this.priceGoods(); //сумма товаров
         this.result(); //вывод суммы
     }
 
-    fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'Клубничка', price: 2000 },
-            { id: 2, title: 'Ягодка', price: 1900 },
-            { id: 3, title: 'Малинка', price: 1800 },
-            { id: 4, title: 'Черничка', price: 1500 },
-        ]
+    _getProducts() {
+        return fetch(`${API}/catalogData.json`) // тут название файла
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
     }
+
+    // fetchProducts() {
+    //     this.goods = [
+    //         { id: 1, title: 'Клубничка', price: 2000 },
+    //         { id: 2, title: 'Ягодка', price: 1900 },
+    //         { id: 3, title: 'Малинка', price: 1800 },
+    //         { id: 4, title: 'Черничка', price: 1500 },
+    //     ]
+    // }
 
     render() { //вывод товаров на страницу 
         const block = document.querySelector(this.container);
@@ -43,7 +57,7 @@ class ProductList {
 
 class ProductItem { //отдельный товар
     constructor(product, img = `image/cardProduct.jpg`) {
-        this.title = product.title;
+        this.product_name = product.product_name;
         this.id = product.id;
         this.price = product.price;
         this.img = img;
@@ -52,7 +66,7 @@ class ProductItem { //отдельный товар
     render() { // верстка товара
         return `<div class="product-item">
         <img class="img" src="${this.img}">
-        <h3>${this.title}</h3>
+        <h3>${this.product_name}</h3>
         <p>${this.price} руб.</p>
         <button class="buy-btn">Купить</button>
     </div>`
