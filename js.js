fd382243bca6254eaa1e5e3ce3f0d18475e101ac
romanchik4261 <romanchik4261@mail.ru>
2021-09-09 16:31:24 +0300

    добавлена фильтрация

--- js.js
+++ js.js
@@ -5,10 +5,14 @@ const app = new Vue({
 
     data: {
         catalogUrl: '/catalogData.json',
+        cartUrl: '/getBasket.json',
+        cartItems: [],
         products: [],
+        filtered: [],
         imgCatalog: 'image/cardProduct.jpg',
+        imgCart: 'image/cardProduct.jpg',
         userSearch: '', //для фильтрации
-        show: false // для того чтобы показывать или скрывать корзину
+        show: false // для того чтобы показывать или скрывать корзину, по умолчанию скрыта
     },
 
     methods: {
@@ -20,27 +24,63 @@ const app = new Vue({
                 })
         },
 
-        addProduct(product) {
-            console.log(product.id_product);
-        }
+        addProduct(product) {// добавляем товар в корзину
+            this.getJson(`${API}/addToBasket.json`)//можно не делать эту часть кода, она для демонстрации того, как можно сделать проверку сервера, в этом файле одна инструкция "result": 1
+                .then(data => {
+                    if (data.result === 1) { //после считывания файла, если result равен1, значит доступ к серверу есть
+                        let productId = +element.dataset['id']; // С помощью data - атрибутов считывается id
 
+                        let find = this.products.find(product => product.id_product === productId);//дальше нужно понять полученный товар есть уже в корзине или нет.
+
+                        if (find) { //если такой товар с таким id есть, то добавляется просто количество
+                            find.quantity++;
+                            this._updateCart(find);
+                        } else { //а если такого товара нет, то создаем его
+                            let product = {
+                                id_product: productId,
+                                price: +element.dataset['price'], //полчаем из кнопки через data-атрибут цену и название
+                                product_name: element.dataset['name'],
+                                quantity: 1
+                            };
+                            this.goods = [product]; //записываем в массив полученный элемент
+                            this.render(); //вызываем метод и отрисовываем товар на странице
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
+        },
+
+        filter() {
+            let regexp = new RegExp(this.userSearch, 'i'); // рег. выражение проверяет название каждого товара на соответствие введенному значению в инпут, регист не учитывается
+            this.filtered = this.products.filter(el => regexp.test(el.product_name)); // обходим массив всех товаров и из товара берем название товара, проверяем название на соответствие рег. выражению и такие товары помещаем в массив filtered (массив содержащий элементы соответствующие правилу)
+        }
     },
 
+
+
     mounted() { //при создании объекта запускается этот метод
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for (let item of data.contents) {
+                    this.cartItems.push(item);
+                }
+            });
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
                 for (let el of data) {
                     this.products.push(el);
+                    this.$data.filtered.push(el); //можно к свойствам из data обращаться не только через this, но и через $, это тоже работает. Если нет локального data можно обращаться через this
                 }
             });
         this.getJson(`getProducts.json`) //локальный файл
             .then(data => {
                 for (let el of data) {
                     this.products.push(el);
+                    this.filtered.push(el);
                 }
             })
-    }
-
+    },
 
 })
 
